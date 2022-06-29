a4ad91e265d5c37ae22d0cc3c98e830ffb6669fd
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-06-29 17:53:25 +0200

    feat: :sparkles: add daily background job

--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
-﻿using ITECHAutoAttendance;
+﻿using Hangfire;
+using ITECHAutoAttendance;
 
-new AutoAttendance().Run();
+GlobalConfiguration.Configuration.UseInMemoryStorage();
+const string cronExpression = "0 9 * * *"; // Every day at 9 am
+var autoAttendance = new AutoAttendance();
+
+RecurringJob.AddOrUpdate(() => autoAttendance.Run(), cronExpression);
+
+using var _ = new BackgroundJobServer();
+Console.WriteLine("Hangfire Server started. Press any key to exit...");
+Console.ReadKey();
