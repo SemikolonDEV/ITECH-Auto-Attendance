11150721be6f43d1a93d3bb4dbfb8a348005625e
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-06-29 23:58:13 +0200

    build: :bug: fix build script

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ dockername="itech-auto-attendance";
 docker stop $(docker ps | awk '{split($2,image,":"); print $1, image[1]}' | awk -v image=$dockername '$2 == image {print $1}')
 
 # Build & Start container
-cp '~/projects/Rose-Linode/env/ITECH-Auto-Attendance/appsettings.json' .
+cp ~/projects/Rose-Linode/env/ITECH-Auto-Attendance/appsettings.json .
 docker build -t $dockername . && docker run -it -d $dockername && docker ps
