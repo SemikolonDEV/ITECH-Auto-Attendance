b6da4f4754a03a6b5f4abb2489f7b2407c80e5e3
Solomon Rosemite™ <SolomonRosemiteProjects@gmail.com>
2022-12-02 09:52:40 +0100

    feat: 📝 improve logging date format

--- Startup.cs
+++ Startup.cs
@@ -10,11 +10,17 @@ namespace ITECHAutoAttendance;
 
 public static class Startup
 {
+    private const string SerilogOutputTemplate =
+        "{Timestamp:yyyy'-'MM'-'dd'T'HH':'mm':'ss.ffffff zzz} [{Level:u3}] {SourceContext} - {Message:lj}{NewLine}{Exception}";
+
+    
     public static IHostBuilder CreateHostBuilder() =>
         Host.CreateDefaultBuilder()
             .UseSerilog((context, configuration) =>
             {
-                configuration.WriteTo.Console();
+                configuration.WriteTo.Console(
+                    outputTemplate: SerilogOutputTemplate
+                );
             })
             .ConfigureServices((context, services) =>
             {
